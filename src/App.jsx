64bcd40fae8f67dc1a4bd68d9eb8c2f7029e465c
Ujef <ujef.kelawala123@gmail.com>
2024-09-16 18:23:19 +0530

    add project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <h1 className="text-3xl text-center mt-3 font-bold underline">
+      Shopping Cart!!!!
+    </h1>
+  )
+}
+
+export default App;
