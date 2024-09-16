b203ea9478a92dda4d230538a0d5a581663fe210
Ujef <ujef.kelawala123@gmail.com>
2024-09-17 00:27:04 +0530

    call header and products component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import Products from './components/Products';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1 className="text-3xl text-center mt-3 font-bold underline">
-      Shopping Cart!!!!
-    </h1>
+    <>
+      <Header />
+      <Products />
+    </>
   )
 }
 
