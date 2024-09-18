79d0ed42d63832a33b55df49688414f838934bd6
Ujef <ujef.kelawala123@gmail.com>
2024-09-18 14:26:14 +0530

    add cart state via CartContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,12 @@
 import { IoCart } from "react-icons/io5";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+
 function Header() {
+
+
+    const { cart } = useContext(CartContext);
+
     return (
         <header className='text-center mt-3 font-bold flex justify-between max-w-4xl mx-auto'>
             <h1>
@@ -7,7 +14,7 @@ function Header() {
             </h1>
             <button className='bg-gray-100 border-l-2 border-stone-700 flex items-center gap-2 px-3 py-1 rounded-md'>
                 <IoCart />
-                <span>4</span>
+                <span>{cart.length}</span>
             </button>
         </header>
     )
