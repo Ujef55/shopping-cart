42ddce7ab484a5b05796475a63e855ac50487557
Ujef <ujef.kelawala123@gmail.com>
2024-09-19 21:29:52 +0530

    render page conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import Products from './components/Products';
 import Header from './components/Header';
+import Cart from './components/Cart';
+import { useContext } from 'react';
+import { CartContext } from './context/CartContext';
 
 function App() {
+
+  const { cartPage } = useContext(CartContext);
+
+  let content;
+
+  if (cartPage) {
+    content = <Cart />
+  } else if (!cartPage) {
+    content = <Products />
+  }
+
   return (
     <>
       <Header />
-      <Products />
+      {content}
     </>
   )
 }
